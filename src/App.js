e82a909ed7441b6ca7bd9264c158a181f23e2ab7
HoangVu <hoangvu1991uit@gmail.com>
2020-03-12 16:35:49 +0700

    [Basics in React] [Unidirectional Data Flow] Format

--- src/App.js
+++ src/App.js
@@ -43,21 +43,15 @@ class App extends Component {
         return (
             <div className="App">
                 {this.state.list.map(item =>
-                        <div key={item.objectID}>
-            <span>
-              <a href={item.url}>{item.title}</a>
-            </span>
-                            <span>{item.author}</span>
-                            <span>{item.num_comments}</span>
-                            <span>{item.points}</span>
-                            <span>
-              <button
-                  onClick={() => this.onDismiss(item.objectID)}
-                  type="button">
-                Dismiss
-              </button>
-            </span>
-                        </div>
+                    <div key={item.objectID}>
+                        <span><a href={item.url}>{item.title}</a></span>
+                        <span>{item.author}</span>
+                        <span>{item.num_comments}</span>
+                        <span>{item.points}</span>
+                        <span>
+                            <button onClick={() => this.onDismiss(item.objectID)} type="button"> Dismiss</button>
+                        </span>
+                    </div>
                 )}
             </div>
         );
