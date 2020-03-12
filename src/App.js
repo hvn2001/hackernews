e7dea0389de1cc2807dfb5c8213bd1c393dc71aa
HoangVu <hoangvu1991uit@gmail.com>
2020-03-12 20:32:51 +0700

    [Basics in React] [Split Up Components] Search

--- src/App.js
+++ src/App.js
@@ -31,6 +31,19 @@ function _isSearched(searchTerm) {
 const isSearched = (searchTerm) => (item) =>
     item.title.toLowerCase().includes(searchTerm.toLowerCase());
 
+class Search extends Component {
+    render() {
+        const {value, onChange} = this.props;
+        return <form>
+            <input
+                type="text"
+                value={value}
+                onChange={onChange}
+            />
+        </form>;
+    }
+}
+
 class App extends Component {
 
     constructor(props) {
@@ -56,16 +69,10 @@ class App extends Component {
     }
 
     render() {
-        const { searchTerm, list } = this.state;
+        const {searchTerm, list} = this.state;
         return (
             <div className="App">
-                <form>
-                    <input
-                        type="text"
-                        value={searchTerm}
-                        onChange={this.onSearchChange}
-                    />
-                </form>
+                <Search value={searchTerm} onChange={this.onSearchChange}/>
                 {this.state.list.filter(_isSearched(this.state.searchTerm)).map(item =>
                     <div key={item.objectID}>
                         <span><a href={item.url}>{item.title}</a></span>
