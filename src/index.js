8245c09f523f648387ef0c1631d8e921473f2959
HoangVu <hoangvu1991uit@gmail.com>
2020-03-12 21:49:21 +0700

    [Basics in React] [Component Declarations]

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-// import App from './App';
-import App from './BasicsReact/EventHandler/app3';
+import App from './App';
+// import App from './BasicsReact/EventHandler/app3';
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(<App/>, document.getElementById('root'));
