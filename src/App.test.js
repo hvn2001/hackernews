c49fc1c9988af746dcc88bfdcfc1bbe752c05883
HoangVu <hoangvu1991uit@gmail.com>
2020-03-13 18:06:00 +0700

    [Testing] [Tests with Jest] Testing Table component

--- src/App.test.js
+++ src/App.test.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import renderer from 'react-test-renderer';
-import App, {Search, Button} from './App';
+import App, {Search, Button, Table} from './App';
 
 describe('App', () => {
 
@@ -53,4 +53,28 @@ describe('Button', () => {
         expect(tree).toMatchSnapshot();
     });
 
+});
+
+
+describe('Table', () => {
+
+    const props = {
+        list: [
+            {title: '1', author: '1', num_comments: 1, points: 2, objectID: 'y'},
+            {title: '2', author: '2', num_comments: 1, points: 2, objectID: 'z'},
+        ],
+    };
+
+    it('renders without crashing', () => {
+        const div = document.createElement('div');
+        ReactDOM.render(<Table {...props} />, div);
+    });
+
+    test('has a valid snapshot', () => {
+        const component = renderer.create(
+            <Table {...props} />
+        );
+        const tree = component.toJSON();
+        expect(tree).toMatchSnapshot();
+    });
 });
