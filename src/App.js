5eb83d2b29f408fa6a5583e22be9956e465f8206
HoangVu <hoangvu1991uit@gmail.com>
2020-03-14 10:52:03 +0700

    [Advanced Components] [Reverse sort] make visual

--- src/App.js
+++ src/App.js
@@ -59,13 +59,21 @@ class Search extends Component {
     );
 }*/
 
-const Sort = ({sortKey, onSort, children}) =>
-    <Button
-        onClick={() => onSort(sortKey)}
-        className="button-inline"
-    >
-        {children}
-    </Button>
+const Sort = ({sortKey, onSort, children, activeSortKey}) => {
+    const sortClass = ['button-inline'];
+
+    if (sortKey === activeSortKey) {
+        sortClass.push('button-active');
+    }
+    return (
+        <Button
+            onClick={() => onSort(sortKey)}
+            className={sortClass.join(' ')}
+        >
+            {children}
+        </Button>
+    );
+}
 
 class Table extends Component {
     render() {
